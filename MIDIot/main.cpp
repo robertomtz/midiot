61f4dafeddcad49427998e017bcac91000fcdd6c
Roberto Mtz <roberto@robertomtz.com>
2015-04-06 23:30:06 -0500

    Start the machine

---
+++ MIDIot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MIDIot
+//
+//  Created by Roberto Mtz on 4/6/15.
+//  Copyright (c) 2015 ITESM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
