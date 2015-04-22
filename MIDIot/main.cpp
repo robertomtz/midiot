50076bc158f958165c8300e4584cbb555cccd2fb
Adrian Rangel <adrian.rangel@gmail.com>
2015-04-22 03:47:12 -0500

    red background, fa & sol textures

--- MIDIot/main.cpp
+++ MIDIot/main.cpp
@@ -48,12 +48,17 @@ bool entraUno= false;
 
 void loadTexture(Image* image,int k)
 {
-    
     glBindTexture(GL_TEXTURE_2D, texName[k]); //Tell OpenGL which texture to edit
-    
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-    
+
     //Map the image to the texture
     glTexImage2D(GL_TEXTURE_2D,                //Always GL_TEXTURE_2D
                  0,                            //0 for now
@@ -68,19 +73,22 @@ void loadTexture(Image* image,int k)
 
 void initRendering()
 {
-    int i=0;
+    GLuint i=0;
     glEnable(GL_DEPTH_TEST);
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
+//    glEnable(GL_LIGHTING);
+//    glEnable(GL_LIGHT0);
     glEnable(GL_NORMALIZE); ///Users/mariaroque/Imagenes
     // glEnable(GL_COLOR_MATERIAL);
+
     glGenTextures(5, texName); //Make room for our texture
-    Image* image = loadBMP("sol.bmp");
-    loadTexture(image,i++);
+    Image* image;
+    image = loadBMP("/Users/Adrian/Copy/ITC/8to-Semestre/Graficas/midiot/MIDIot/sol.bmp");
+    loadTexture(image, 0);
+
+    image = loadBMP("/Users/Adrian/Copy/ITC/8to-Semestre/Graficas/midiot/MIDIot/fa.bmp");
+    loadTexture(image, 1);
     delete image;
 }
-
-
 
 int getRanNumber() {
     return rand() % 24;
@@ -170,15 +178,6 @@ void myTimer(int v)
     glutTimerFunc(10, myTimer, 1);
 }
 
-void init()
-{
-    // Para que las paredes se vean sólidas (no transparentes)
-    glEnable(GL_DEPTH_TEST);
-    
-    glEnable(GL_BLEND);
-}
-
-
 std::string cToString(char a){
     std::stringstream ss;
     ss << a;
@@ -223,64 +222,102 @@ void dibuja()
 {
     //puntos actuales jugador y dealer
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_TEXTURE_2D);
+
+
     glLineWidth(2);
     glColor3f(0.0, 0.0, 0.0);
-    
-    if (entrar){
-    glBindTexture(GL_TEXTURE_2D, texName[1]);
-    glRectd(-470, 440, -380, 320);
-    glRectd(-470, 265, -380, 145);
-    
-    glBegin(GL_LINES);
-    for (int i=0; i<10; i++) {
-        if(i<5){
-            glVertex3f(-480, 430-i*25, 0);
-            glVertex3f(480, 430-i*25, 0);
-        }else{
-            glVertex3f(-480, 430-i*25-50, 0);
-            glVertex3f(480, 430-i*25-50, 0);
-        }
-    }
-    glEnd();
-    
-    //Dibuja todas las notas
-//    for (int i=0; i<25; i++) {
-//        glPushMatrix();
-//        glTranslatef(posXNotes+40*i, notaCordenada[i], 0);
-//        if(notaNombre[i].find("#")!=-1){
-//            drawText(50, 100, .15, "#", GLUT_BITMAP_9_BY_15);
-//        }
-//        glutSolidSphere(12,12, 12);
-//        glPopMatrix();
-//    }
-    
-    if(start){
-        glPushMatrix();
-        glTranslatef(posXNotes, notaCordenada[notaActual], 0);
-        if(notaNombre[notaActual].find("#")!=-1){
-            drawText(50, 100, .15, "#", GLUT_BITMAP_9_BY_15);
-        }
-        glutSolidSphere(12,12, 12);
-        glPopMatrix();
-    }
 
-    drawText(-2000, 1850, 0.25, portName, GLUT_BITMAP_9_BY_15);
-    drawText(1300, 1850, 0.25, "MIDI OT", GLUT_BITMAP_9_BY_15);
+    if (entrar){
+
+        glColor3f(1.0, 1.0, 1.0);
+        glBindTexture(GL_TEXTURE_2D, texName[0]);
+        glBegin(GL_QUADS);
+
+        glTexCoord2f(0.0f, 1.0f);
+        glVertex3f(-470.0f, 440, 0);
+
+        glTexCoord2f(0.0f, 0.0f);
+        glVertex3f(-470.0f, 320.0f, 0);
+
+        glTexCoord2f(1.0f, 0.0f);
+        glVertex3f(-380.0f, 320.0f, 0);
+
+        glTexCoord2f(1.0f, 1.0f);
+        glVertex3f(-380.0f, 440.0f, 0);
+        glEnd();
+
+        glBindTexture(GL_TEXTURE_2D, texName[1]);
+        glBegin(GL_QUADS);
+
+        glTexCoord2f(0.0f, 1.0f);
+        glVertex3f(-470.0f, 265, 0);
+
+        glTexCoord2f(0.0f, 0.0f);
+        glVertex3f(-470.0f, 145.0f, 0);
+
+        glTexCoord2f(1.0f, 0.0f);
+        glVertex3f(-380.0f, 145.0f, 0);
+
+        glTexCoord2f(1.0f, 1.0f);
+        glVertex3f(-380.0f, 265.0f, 0);
+        glEnd();
+
+
+        glColor3f(0.0, 0.0, 0.0);
+//        glRectd(-470, 440, -380, 320);
+//        glRectd(-470, 265, -380, 145);
+
+        glBegin(GL_LINES);
+        for (int i=0; i<10; i++) {
+            if(i<5){
+                glVertex3f(-480, 430-i*25, 0);
+                glVertex3f(480, 430-i*25, 0);
+            }else{
+                glVertex3f(-480, 430-i*25-50, 0);
+                glVertex3f(480, 430-i*25-50, 0);
+            }
+        }
+        glEnd();
     
-    if(oprimidoMidi && (!entraUno)){
-        entraUno=true;
-        if ((int)message[1]-48>=0) {
-            drawText(-100, -250, 1, notaNombre[notaOprimidaActual], GLUT_BITMAP_9_BY_15);
-            if(start){
+        //Dibuja todas las notas
+    //    for (int i=0; i<25; i++) {
+    //        glPushMatrix();
+    //        glTranslatef(posXNotes+40*i, notaCordenada[i], 0);
+    //        if(notaNombre[i].find("#")!=-1){
+    //            drawText(50, 100, .15, "#", GLUT_BITMAP_9_BY_15);
+    //        }
+    //        glutSolidSphere(12,12, 12);
+    //        glPopMatrix();
+    //    }
+        
+        if(start){
+            glPushMatrix();
+            glTranslatef(posXNotes, notaCordenada[notaActual], 0);
+            if(notaNombre[notaActual].find("#")!=-1){
+                drawText(50, 100, .15, "#", GLUT_BITMAP_9_BY_15);
+            }
+            glutSolidSphere(12,12, 12);
+            glPopMatrix();
+        }
+
+        drawText(-2000, 1850, 0.25, portName, GLUT_BITMAP_9_BY_15);
+        drawText(1300, 1850, 0.25, "MIDI OT", GLUT_BITMAP_9_BY_15);
+        
+        if(oprimidoMidi && (!entraUno)){
+            entraUno=true;
+            if ((int)message[1]-48>=0) {
+                drawText(-100, -250, 1, notaNombre[notaOprimidaActual], GLUT_BITMAP_9_BY_15);
+                if(start){
+                    drawText(-400, -250, 1, notaNombre[notaActual], GLUT_BITMAP_9_BY_15);
+                }
+                playSound();
                 drawText(-400, -250, 1, notaNombre[notaActual], GLUT_BITMAP_9_BY_15);
             }
-            playSound();
-            drawText(-400, -250, 1, notaNombre[notaActual], GLUT_BITMAP_9_BY_15);
         }
-    }
-    
-    drawText(-500, -480, 1, toString(tiempo), GLUT_BITMAP_9_BY_15); //time
-    drawText(100, -480, 1, toString(score), GLUT_BITMAP_9_BY_15); //score
+        
+        drawText(-500, -480, 1, toString(tiempo), GLUT_BITMAP_9_BY_15); //time
+        drawText(100, -480, 1, toString(score), GLUT_BITMAP_9_BY_15); //score
     } else{
         glColor3f(1, 1, 1);
         glLineWidth(6);
@@ -297,7 +334,7 @@ void dibuja()
     }
     
 //    rectangulo fondo blanco
-    glColor3f(1, 1, 1);
+    glColor3f(0.53, 0.17, 0.18);
     glRectd(-an, -an, an, an);
     
     
@@ -363,8 +400,9 @@ int main(int argc, char *argv[])
     glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH| GLUT_DOUBLE );
     glutCreateWindow("MIDIot A01190757 - A01190871");
-    init();
-    glEnable(GL_DEPTH_TEST);
+    initRendering();
+    glClearColor(1.0,1.0,1.0,1.0);
+//    glEnable(GL_DEPTH_TEST);
     glutKeyboardFunc(myKey);
     glutDisplayFunc(dibuja);
     glutTimerFunc(5, myTimer, 1);
@@ -372,211 +410,3 @@ int main(int argc, char *argv[])
     glutMainLoop();
     return 0;
 }
-
-
-
-#include <assert.h>
-#include <fstream>
-
-#include "imageloader.h"
-
-using namespace std;
-
-Image::Image(char* ps, int w, int h) : pixels(ps), width(w), height(h)
-{
-    
-}
-
-Image::~Image()
-{
-    delete[] pixels;
-}
-
-namespace
-{
-    //Converts a four-character array to an integer, using little-endian form
-    int toInt(const char* bytes)
-    {
-        return (int)(((unsigned char)bytes[3] << 24) |
-                     ((unsigned char)bytes[2] << 16) |
-                     ((unsigned char)bytes[1] << 8) |
-                     (unsigned char)bytes[0]);
-    }
-    
-    //Converts a two-character array to a short, using little-endian form
-    short toShort(const char* bytes)
-    {
-        return (short)(((unsigned char)bytes[1] << 8) |
-                       (unsigned char)bytes[0]);
-    }
-    
-    //Reads the next four bytes as an integer, using little-endian form
-    int readInt(ifstream &input)
-    {
-        char buffer[4];
-        input.read(buffer, 4);
-        return toInt(buffer);
-    }
-    
-    //Reads the next two bytes as a short, using little-endian form
-    short readShort(ifstream &input)
-    {
-        char buffer[2];
-        input.read(buffer, 2);
-        return toShort(buffer);
-    }
-    
-    //Just like auto_ptr, but for arrays
-    template<class T>
-    class auto_array
-    {
-    private:
-        T* array;
-        mutable bool isReleased;
-    public:
-        explicit auto_array(T* array_ = NULL) :
-        array(array_), isReleased(false)
-        {
-        }
-        
-        auto_array(const auto_array<T> &aarray)
-        {
-            array = aarray.array;
-            isReleased = aarray.isReleased;
-            aarray.isReleased = true;
-        }
-        
-        ~auto_array()
-        {
-            if (!isReleased && array != NULL)
-            {
-                delete[] array;
-            }
-        }
-        
-        T* get() const
-        {
-            return array;
-        }
-        
-        T &operator*() const
-        {
-            return *array;
-        }
-        
-        void operator=(const auto_array<T> &aarray)
-        {
-            if (!isReleased && array != NULL)
-            {
-                delete[] array;
-            }
-            array = aarray.array;
-            isReleased = aarray.isReleased;
-            aarray.isReleased = true;
-        }
-        
-        T* operator->() const
-        {
-            return array;
-        }
-        
-        T* release()
-        {
-            isReleased = true;
-            return array;
-        }
-        
-        void reset(T* array_ = NULL)
-        {
-            if (!isReleased && array != NULL)
-            {
-                delete[] array;
-            }
-            array = array_;
-        }
-        
-        T* operator+(int i)
-        {
-            return array + i;
-        }
-        
-        T &operator[](int i)
-        {
-            return array[i];
-        }
-    };
-}
-
-Image* loadBMP(const char* filename)
-{
-    ifstream input;
-    input.open(filename, ifstream::binary);
-    assert(!input.fail() || !"Could not find file");
-    char buffer[2];
-    input.read(buffer, 2);
-    assert(buffer[0] == 'B' && buffer[1] == 'M' || !"Not a bitmap file");
-    input.ignore(8);
-    int dataOffset = readInt(input);
-    
-    //Read the header
-    int headerSize = readInt(input);
-    int width;
-    int height;
-    switch (headerSize)
-    {
-        case 40:
-            //V3
-            width = readInt(input);
-            height = readInt(input);
-            input.ignore(2);
-            assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
-            assert(readShort(input) == 0 || !"Image is compressed");
-            break;
-        case 12:
-            //OS/2 V1
-            width = readShort(input);
-            height = readShort(input);
-            input.ignore(2);
-            assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
-            break;
-        case 64:
-            //OS/2 V2
-            assert(!"Can't load OS/2 V2 bitmaps");
-            break;
-        case 108:
-            //Windows V4
-            assert(!"Can't load Windows V4 bitmaps");
-            break;
-        case 124:
-            //Windows V5
-            assert(!"Can't load Windows V5 bitmaps");
-            break;
-        default:
-            assert(!"Unknown bitmap format");
-    }
-    
-    //Read the data
-    int bytesPerRow = ((width * 3 + 3) / 4) * 4 - (width * 3 % 4);
-    int size = bytesPerRow * height;
-    auto_array<char> pixels(new char[size]);
-    input.seekg(dataOffset, ios_base::beg);
-    input.read(pixels.get(), size);
-    
-    //Get the data into the right format
-    auto_array<char> pixels2(new char[width * height * 3]);
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            for (int c = 0; c < 3; c++)
-            {
-                pixels2[3 * (width * y + x) + c] =
-                pixels[bytesPerRow * y + 3 * x + (2 - c)];
-            }
-        }
-    }
-    
-    input.close();
-    return new Image(pixels2.release(), width, height);
-}
-
